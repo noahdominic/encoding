b0723bf10c27db9023e7fa071486b1e722e57acd
noahdominic <ndsilvio@gmail.com>
2018-10-10 20:57:32 +0800

    Complete encoding algorithms (rz, nrz, nrzi)

---
+++ encoder.py
@@ -0,0 +1,35 @@
+# Copyright (c) Noah Dominic Silvio <ndsilvio@gmail.com>
+# version 1.0.0
+
+# rz() [stable as of version 1.0.0]
+# accepts array inputbytes
+# returns encodedbytes in rz unipolar encoding
+def rz(inputbytes):
+    outputbytes = []
+    for i in inputbytes:
+        outputbytes.append((i > 0) * 5)
+    return outputbytes
+
+
+# nrz() [stabl as of version 1.0.0]
+# accepts array inputbytes
+# returns encodedbytes in nrz bipolar line encoding
+def nrz(inputbytes):
+    outputbytes = []
+    for i in inputbytes:
+        outputbytes.append(10 * (i > 0) - 5)
+    return outputbytes
+
+
+# nrzi() [stable as of version 1.0.0]
+# accepts array inputbytes
+# returns encodedbytes in nrz bipolar inverted encoding
+def nrzi(inputbytes):
+    outputbytes = []
+    tempbyte = 0
+    for i in inputbytes:
+        # nextbyte = 10 * (i == 1) * (not inputbytes)- 5
+        nextbyte = (tempbyte) != (i > 0)
+        outputbytes.append(10 * nextbyte - 5)
+        tempbyte = nextbyte
+    return outputbytes
---
+++ main.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2018 Noah Dominic Silvio <ndsilvio@gmail.com>
+# version 1.0.0
+
+import matplotlib.pyplot as plt
+import encoder
+
+print(encoder.rz([1,0,0,1, 0.9]))
+print(encoder.nrz([1,0,0,1, 0.9]))
+print(encoder.nrzi([0,1,0,1,0,1,1,0,0,1]))
